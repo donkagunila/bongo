c09e694c63d51760eb232fc72e8b8c0563f77245
donald kagunila <donkagunila@gmail.com>
2020-05-19 09:23:29 +0300

    add foter

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -306,40 +306,40 @@
 
 
 			<section class="c-block" id="bt-sec">
-			<div class="container">
-				<div class="about-us-sec">
-					<div class="about-us">
-						<div class="row">
-							<div class="col-lg-6">
-								<div class="about-text">
-									<h2>BONGO TOURS</h2>
-									<h3>WHO WE ARE </h3>
-									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-									<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-									<a href="#" title="" class="lnk-default">About Us <i class="la la-arrow-right"></i></a>
-								</div><!--about-text end-->
+				<div class="container">
+					<div class="about-us-sec">
+						<div class="about-us">
+							<div class="row">
+								<div class="col-lg-6">
+									<div class="about-text">
+										<h2>BONGO TOURS</h2>
+										<h3>WHO WE ARE </h3>
+										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+										<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+										<a href="#" title="" class="lnk-default">About Us <i class="la la-arrow-right"></i></a>
+									</div><!--about-text end-->
+								</div>
+								<div class="col-lg-6">
+									<div class="abt_carousel">
+										<div class="abt-img">
+											<img src="images/resources/abt-img.jpg" alt="">
+										</div><!--abt-img end-->
+										<div class="abt-img">
+											<img src="images/resources/abt_img2.jpg" alt="">
+										</div><!--abt-img end-->
+										<div class="abt-img">
+											<img src="images/resources/abt_img3.jpg" alt="">
+										</div><!--abt-img end-->
+										<div class="abt-img">
+											<img src="images/resources/abt_img4.jpg" alt="">
+										</div><!--abt-img end-->
+									</div><!--abt_carousel end-->
+								</div>
 							</div>
-							<div class="col-lg-6">
-								<div class="abt_carousel">
-									<div class="abt-img">
-										<img src="images/resources/abt-img.jpg" alt="">
-									</div><!--abt-img end-->
-									<div class="abt-img">
-										<img src="images/resources/abt_img2.jpg" alt="">
-									</div><!--abt-img end-->
-									<div class="abt-img">
-										<img src="images/resources/abt_img3.jpg" alt="">
-									</div><!--abt-img end-->
-									<div class="abt-img">
-										<img src="images/resources/abt_img4.jpg" alt="">
-									</div><!--abt-img end-->
-								</div><!--abt_carousel end-->
-							</div>
-						</div>
-					</div><!--about-us end-->
-				</div><!--about-us-sec end-->
-			</div>
-		</section><!--ABOUT-SEC END-->
+						</div><!--about-us end-->
+					</div><!--about-us-sec end-->
+				</div>
+			</section><!--ABOUT-SEC END-->
 
 
 
@@ -456,5 +456,312 @@
 				</div><!--services-sec end-->
 			</div>
 		</section><!--SERVICES SEC END-->
+
+
+
+
+		<section class="main-slider relax-sec">
+	        <div id="rev_slider_476_1_wrapper2" class="rev_slider_wrapper fullscreen-container" data-alias="christmas-snow-scene" data-source="gallery" style="background-color:transparent;padding:0px;">
+	        <!-- START REVOLUTION SLIDER 5.3.0.2 fullscreen mode -->
+	            <div id="rev_slider_476_2" class="rev_slider" style="display:none;" data-version="5.3.0.2">
+	                <ul>    
+	                    <!-- SLIDE  -->
+	                    <li data-index="rs-1648" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="1000" data-thumb="images/slider_banner.jpg" data-rotate="0" data-saveperformance="off" data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+
+							<img src="images/resources/bg1.jpg" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="" class="rev-slidebg" data-no-retina>
+	                        <!-- LAYERS -->
+	                        <div class="tp-caption   tp-resizeme fullscreenvideo  disable-layer tp-videolayer" 
+			 					id="lordcros-video-bg" 
+			 					data-x="0" 
+			 					data-y="0" 
+								data-whitespace="nowrap"
+								data-type="video" 
+								data-responsive_offset="on" 
+								data-frames='[{"delay":10,"speed":300,"frame":"0","from":"opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+									data-ytid="7RVH26KaVDg" data-videoattributes="version=3&amp;enablejsapi=1&amp;html5=1&amp;hd=1&wmode=opaque&showinfo=0&rel=0;;origin=https://lordcros.c-themes.com;" data-videorate="1" data-videowidth="100%" data-videoheight="100%" data-videocontrols="none" data-videoloop="none" data-forceCover="1" data-aspectratio="16:9"			data-textAlign="['inherit','inherit','inherit','inherit']"
+								data-paddingtop="[0,0,0,0]"
+								data-paddingright="[0,0,0,0]"
+								data-paddingbottom="[0,0,0,0]"
+								data-paddingleft="[0,0,0,0]"
+								data-autoplay="off" 
+								data-volume="mute"			
+								data-allowfullscreenvideo="true" 
+								style="z-index: 5;"> 
+							</div>
+
+							<!-- LAYER NR. 3 -->
+
+							<h2 class="tp-caption   tp-resizeme" 
+								id="slide-2-layer-1" 
+								data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+								data-y="['middle','middle','middle','middle']" data-voffset="['-71','-71','-71','-71']" 
+								data-fontsize="['30','30','30','20']"
+								data-lineheight="['38','38','38','28']"
+								data-width="none"
+								data-height="none"
+								data-whitespace="nowrap"
+								data-type="text" 
+								data-responsive_offset="on" 
+								data-frames='[{"delay":460,"speed":1000,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+								data-textAlign="['center','center','center','center']"
+								data-paddingtop="[0,0,0,0]"
+								data-paddingright="[0,0,0,0]"
+								data-paddingbottom="[0,0,0,0]"
+								data-paddingleft="[0,0,0,0]"
+								style="z-index: 7; white-space: nowrap; font-size: 30px; line-height: 38px; font-weight: 400; color: #ffffff; letter-spacing: 0px;font-family:Roboto Slab;text-transform:uppercase;">Relax And Enjoy Your Stay 
+							</h2>
+
+							<!-- LAYER NR. 4 -->
+
+							<div class="tp-caption tp-resizeme" 
+								id="slide-2-layer-2" 
+								data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
+								data-y="['middle','middle','middle','middle']" data-voffset="['-34','-34','-34','-34']" 
+								data-width="none"
+								data-height="none"
+								data-whitespace="nowrap"
+								data-type="text" 
+								data-responsive_offset="on" 
+								data-frames='[{"delay":1090,"speed":1000,"frame":"0","from":"y:50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+								data-textAlign="['center','center','center','center']"
+								data-paddingtop="[0,0,0,0]"
+								data-paddingright="[0,0,0,0]"
+								data-paddingbottom="[0,0,0,0]"
+								data-paddingleft="[0,0,0,0]"
+								style="z-index: 8; white-space: nowrap; font-size: 15px; line-height: 20px; font-weight: 400; color: #ffffff; letter-spacing: 0px;font-family:Roboto;text-transform:uppercase;">The best tourism experience </div>
+							
+							<!-- LAYER NR. 5 -->
+		
+							<div class="tp-caption vid_play tp-resizeme" 
+								id="slide-2-layer-3" 
+								data-x="center" data-hoffset="-10"
+								data-y="center" data-voffset="50" 
+							    data-width="none"
+								data-height="none"
+								data-whitespace="nowrap"
+								data-type="text" 
+								data-actions='[{"event":"click","action":"toggleclass","layer":"lordcros-video-bg","delay":"0","classname":"disable-layer"},{"event":"click","action":"playvideo","layer":"lordcros-video-bg","delay":"500"}]'
+								data-responsive_offset="on" 
+								data-frames='[{"delay":1500,"speed":800,"frame":"0","from":"opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+								data-textAlign="['center','center','center','center']"
+								data-paddingtop="[0,0,0,0]"
+								data-paddingright="[0,0,0,0]"
+								data-paddingbottom="[0,0,0,0]"
+								data-paddingleft="[0,0,0,0]"
+								style="z-index: 10;"><div class="lordcros-video-btn"> <img src="images/icon-play.png" alt=""> </div>
+							</div>
+	                    </li>
+	                </ul>
+	            </div>
+	        </div><!--Revolution Slider end-->
+		</section><!--MAIN-SLIDER END-->
+
+
+		<section class="c-block">
+			<div class="container">
+				<div class="sec-title">
+					<h3>Upcoming events</h3>
+				</div><!--sec-title end-->
+				<div class="blog-items">
+					<div class="row blog_carousel">
+						<div class="col-lg-4 col-md-6">
+							<div class="blog">
+								<div class="blog-img">
+									<img src="images/resources/blog4.jpg" alt="">
+									<a href="#" title="" class="blog-category">June 30</a>
+								</div>
+								<div class="blog-info">
+									<h3 class="post-title"><a href="#" title="">Explore Serengeti</a></h3>
+									<p>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et. Quisque euismod orci ut et lobortis. </p>
+									<a href="#" title="">Read More <i class="la la-arrow-right"></i></a>
+								</div>
+							</div><!--blog end-->
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="blog">
+								<div class="blog-img">
+									<img src="images/resources/blog5.jpg" alt="">
+									<a href="#" title="" class="blog-category">Mar 23, 2021</a>
+								</div>
+								<div class="blog-info">
+									<h3 class="post-title"><a href="#" title="">Visit Malawi</a></h3>
+									<p>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et. Quisque euismod orci ut et lobortis. </p>
+									<a href="#" title="">Read More <i class="la la-arrow-right"></i></a>
+								</div>
+							</div><!--blog end-->
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="blog">
+								<div class="blog-img">
+									<img src="images/resources/blog6.jpg" alt="">
+									<a href="#" title="" class="blog-category">Zanzibar Tour</a>
+								</div>
+								<div class="blog-info">
+									<h3 class="post-title"><a href="#" title="">Proin Fermentum</a></h3>
+									<p>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et. Quisque euismod orci ut et lobortis. </p>
+									<a href="#" title="">Read More <i class="la la-arrow-right"></i></a>
+								</div>
+							</div><!--blog end-->
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="blog">
+								<div class="blog-img">
+									<img src="images/resources/blog1.jpg" alt="">
+									<a href="#" title="" class="blog-category">LUXURY HOTEL</a>
+								</div>
+								<div class="blog-info">
+									<h3 class="post-title"><a href="#" title="">Spa Packages</a></h3>
+									<p>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et. Quisque euismod orci ut et lobortis. </p>
+									<a href="#" title="">Read More <i class="la la-arrow-right"></i></a>
+								</div>
+							</div><!--blog end-->
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="blog">
+								<div class="blog-img">
+									<img src="images/resources/blog2.jpg" alt="">
+									<a href="#" title="" class="blog-category">NEWS</a>
+								</div>
+								<div class="blog-info">
+									<h3 class="post-title"><a href="#" title="">Relax Zone</a></h3>
+									<p>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et. Quisque euismod orci ut et lobortis. </p>
+									<a href="#" title="">Read More <i class="la la-arrow-right"></i></a>
+								</div>
+							</div><!--blog end-->
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="blog">
+								<div class="blog-img">
+									<img src="images/resources/blog3.jpg" alt="">
+									<a href="#" title="" class="blog-category">LUXURY HOTEL</a>
+								</div>
+								<div class="blog-info">
+									<h3 class="post-title"><a href="#" title="">Proin Fermentum</a></h3>
+									<p>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et. Quisque euismod orci ut et lobortis. </p>
+									<a href="#" title="">Read More <i class="la la-arrow-right"></i></a>
+								</div>
+							</div><!--blog end-->
+						</div>
+					</div>
+				</div><!--blog-items end-->
+			</div>
+		</section><!--BLOG-SEC END-->
+
+		<footer>
+			<div class="top-footer">
+				<div class="container">
+					<div class="ft-logo">
+						<img src="images/ft-logo.png" alt="">
+						<div class="ft-text">
+							<h2>Bongo Tours</h2>
+							<h3>Travels & Accomodation</h3>
+						</div>
+					</div><!--ft-logo end-->
+					<div class="room-feats-list">
+						<ul>
+							<li>
+								<h3>5</h3>
+								<h4>COUNTRIES</h4>
+							</li>
+							<li>
+								<h3>195</h3>
+								<h4>LOCATIONS</h4>
+							</li>
+							<li>
+								<h3>623</h3>
+								<h4>ROUTES</h4>
+							</li>
+							<li>
+								<h3>143</h3>
+								<h4>STAFF</h4>
+							</li>
+						</ul>
+					</div><!--room-feats-list end-->
+					<div class="social-links">
+						<ul>
+							<li><a href="#" title=""><i class="fa fa-facebook-square"></i></a></li>
+							<li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#" title=""><i class="fa fa-instagram"></i></a></li>
+						</ul>
+					</div><!--social-links end-->
+					<div class="clearfix"></div>
+				</div>
+			</div><!--top-footer end-->
+			<div class="bottom-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-4 col-md-6">
+							<div class="widget widget-info">
+								<h4>+255 652 441 424</h4>
+								<ul class="info-lst">
+									<li>PO Box 1612 Dar es Salaam Tanzania</li>
+									<li><a href="" class="__cf_email__">info@bongotours.com</a></li>
+								</ul>
+								
+							</div><!--widget-info end-->
+						</div>
+						<div class="col-lg-2 col-md-6">
+							<div class="widget widget-links">
+								<h3 class="widget-title">BookIng</h3>
+								<ul>
+									<li><a href="#" title="">Routes</a></li>
+									<li><a href="#" title="">Flights</a></li>
+									<li><a href="#" title="">Spa & Fitness</a></li>
+									<li><a href="#" title="">Travel</a></li>
+								</ul>
+							</div><!--widget-links end-->
+						</div>
+						<div class="col-lg-2 col-md-6">
+							<div class="widget widget-links">
+								<h3 class="widget-title">About</h3>
+								<ul>
+									<li><a href="#" title="">About</a></li>
+									<li><a href="#" title="">Blog & Events</a></li>
+									<li><a href="#" title="">Awards</a></li>
+									<li><a href="#" title="">About Us</a></li>
+									<li><a href="#" title="">Contact Us</a></li>
+								</ul>
+							</div><!--widget-links end-->
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="widget widget-newsletter">
+								<h3 class="widget-title">Newsletter</h3>
+								<p>Subscribe to our newsletter to get <br /> the latest travels plans, routes ,and other latest news stay updated.</p>
+								<form>
+									<input type="email" name="email" placeholder="Enter your email address">
+									<button type="submit"><i class="la la-paper-plane"></i></button>
+								</form>
+							</div><!--widget-newsletter END-->
+						</div>
+					</div>
+				</div>
+			</div><!--bottom-footer end-->
+			<div class="bottom-strip">
+				<div class="container">
+					<p class="float-right">© 2020 Bongo tours</p>
+				</div>
+				<div class="clearfix"></div>
+			</div><!--bottom-strip end-->
+		</footer><!--FOOTER END-->
+
+
+		<div class="search_sec">
+			<a href="#" title="" class="close-search"><i class="la la-close"></i></a>
+			<div class="search_field">
+				<div class="container">
+					<form>
+						<input type="text" name="search" placeholder="Search for...">
+						<button type="submit"><img src="images/search2.png" alt=""></button>
+					</form>
+				</div>
+			</div>
+		</div><!--search_sec end-->
+
+		<a href="#" title="" class="scrollTop"><i class="la la-arrow-up"></i></a>
+
+		<div class="max-cont"></div>
+
+
+
 
 @endsection
